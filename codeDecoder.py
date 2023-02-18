e78e38d57bc02179eb6c56b2ed5395ae2dab33b6
SenyoGela <senogelamariconb@gmail.com>
2023-02-18 22:41:22 +0800

    add binary dictionary

--- codeDecoder.py
+++ codeDecoder.py
@@ -25,7 +25,7 @@ def phonetic(text):
         return ''.join(decrypt[i] for i in text.split())
     return ' '.join(encrypt[i] for i in text.upper())
 
-def vigerene_key(string, key):
+def vigenere_key(string, key):
     key = list(key)
     if len(string) == len(key):
         return(key)
@@ -34,21 +34,28 @@ def vigerene_key(string, key):
             key.append(key[i % len(key)])
     return ("".join(key))
 
-def vigerene_text(string, key):
-    vigereneText = []
+def vigenere_text(string, key):
+    vigenereText = []
     for i in range(len(string)):
         x = (ord(string[i]) +ord(key[i])) % 26
         x += ord('A')
-        vigereneText.append(chr(x))
-    return ("".join(vigereneText))
+        vigenereText.append(chr(x))
+    return ("".join(vigenereText))
 
-def vigerene():
+def vigenere():
     if __name__ == "__main__":
         string = input("Text: ").upper()
         string = string.replace(" ", "")
         keyword = input("Key: ").upper()
-        key = vigerene_key(string, keyword)
-        vigereneText = vigerene_text(string, key)
-        print("Vigerene Code:", vigereneText)
+        key = vigenere_key(string, keyword)
+        vigenereText = vigenere_text(string, key)
+        print("Vigerene Code:", vigenereText)
 
+def binary(text):
+    encrypt = {'A': '01000001', 'B': '01000010', 'C': '01000011', 'D': '01000100', 'E': '01000101', 'F': '01000110',
+               'G': '01000111', 'H': '01001000', 'I': '01001001', 'J': '01001010', 'K': '01001011', 'L': '01001100',
+               'M': '01001101', 'N': '01001110', 'O': '01001111', 'P': '01010000', 'Q': '01010001', 'R': '01010010',
+               'S': '01010011', 'T': '01010100', 'U': '01010101', 'V': '01010110', 'W': '01010111', 'X': '01011000',
+               'Y': '01011001', 'Z': '01011010', ' ': '.....'}
 #print(phonetic(s))
+
