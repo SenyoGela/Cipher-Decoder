b30fc5368fc484483570c00656df18f41ff0ade4
SenyoGela <senogelamariconb@gmail.com>
2023-02-18 21:23:32 +0800

    add phonetic dictionary

--- codeDecoder.py
+++ codeDecoder.py
@@ -13,4 +13,11 @@ def morse(text):
         return ''.join(decrypt[i] for i in text.split())
     return ' '.join(encrypt[i] for i in text.upper())
 
-print(morse(s))
+def phonetic(text):
+    encrypt = {'A':'Alpha', 'B':'Bravo', 'C':'Charlie', 'D':'Delta', 'E':'Echo', 'F':'Foxtrot', 'G':'Golf', 'H':'Hotel',
+               'I':'India','J':'Juliet', 'K':'Kilo', 'L':'Lima', 'M':'Mike', 'N':'November', 'O':'Oscar', 'P':'Papa',
+               'Q':'Quebec', 'R':'Romeo', 'S':'Sierra', 'T':'Tango', 'U':'Uniform', 'V':'Victor', 'W':'Whiskey',
+               'X':'Xray', 'Y':'Yankee', 'Z':'Zulu'}
+
+
+#print(morse(s))
