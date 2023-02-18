96fc7e4490f48c0c61db182a5b17d0e3cf43d5e8
SenyoGela <senogelamariconb@gmail.com>
2023-02-18 21:29:16 +0800

    add inverse phonetic dictionary

--- codeDecoder.py
+++ codeDecoder.py
@@ -17,7 +17,10 @@ def phonetic(text):
     encrypt = {'A':'Alpha', 'B':'Bravo', 'C':'Charlie', 'D':'Delta', 'E':'Echo', 'F':'Foxtrot', 'G':'Golf', 'H':'Hotel',
                'I':'India','J':'Juliet', 'K':'Kilo', 'L':'Lima', 'M':'Mike', 'N':'November', 'O':'Oscar', 'P':'Papa',
                'Q':'Quebec', 'R':'Romeo', 'S':'Sierra', 'T':'Tango', 'U':'Uniform', 'V':'Victor', 'W':'Whiskey',
-               'X':'Xray', 'Y':'Yankee', 'Z':'Zulu'}
+               'X':'Xray', 'Y':'Yankee', 'Z':'Zulu', ' ':'     '}
+
+    decrypt = {value: key for key, value in encrypt.items()}
+
 
 
 #print(morse(s))
